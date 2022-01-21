7f0d3fcfcfe935be88af1793006503f8f3f50eef
MesutRuzgar <mesutruzgar@gmail.com>
2022-01-21 15:55:55 +0300

    Yetki deneme işlemi başarılı

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,5 +1,7 @@
 using Business.Abstract;
 using Business.Concrete;
+using Core.DependencyRoselvers;
+using Core.Extensions;
 using Core.Utilities.IoC;
 using Core.Utilities.Security.Encryption;
 using Core.Utilities.Security.JWT;
@@ -37,7 +39,7 @@ namespace WebAPI
         {
             services.AddControllers();
 
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            
 
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
@@ -55,8 +57,9 @@ namespace WebAPI
                         IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
                     };
                 });
-
-            ServiceTool.Create(services);
+            services.AddDependencyResolvers(new ICoreModule[]{
+                new CoreModule()
+            });
 
 
         }
@@ -74,6 +77,8 @@ namespace WebAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
